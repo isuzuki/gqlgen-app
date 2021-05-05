43f30afee0b9f9249d159f53c994c3263dd20c12
isuzuki <ikkuntech@gmail.com>
2021-05-06 01:09:07 +0900

    add connect db

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module app
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.13.0 // indirect
-	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
+	github.com/99designs/gqlgen v0.13.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- src/main.go
+++ src/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"app/graph"
 	"app/graph/generated"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
 	"net/http"
 	"os"
@@ -22,6 +24,17 @@ func main() {
 
 	http.Handle("/graphql", srv)
 
+	initDB()
+
 	log.Printf("connect to http://localhost:%s/graphql for GraphQL API", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func initDB() {
+	db, err := gorm.Open("postgres", "postgres://gqlgen_user:gqlgen_password@db:5432/gqlgen_db?sslmode=disable")
+	if err != nil {
+		log.Fatalln("connection failed.", err)
+	}
+
+	defer db.Close()
 }
